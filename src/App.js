5f7b9230394a156cd2e3a6e17953273a491ccaf9
kikx0319 <karloestanislao@yahoo.com>
2020-06-25 00:22:02 -0700

    version1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      MobileMenuClicked: false
+    }
+  }
+
+  toggleMobileMenu = () => {
+    this.setState({
+      MobileMenuClicked: !this.state.MobileMenuClicked
+    })
+  }
+
+  render() {
+    return (
+      <div className="background-image">
+        <body>
+          <header className="App-header">
+            <h3 className="bootstrap-logo"> Start Bootstrap </h3>
+            <nav className="nav-tabs">
+              <span> SERVICES </span>
+              <span> PORTFOLIO </span>
+              <span> ABOUT </span>
+              <span> TEAM </span>
+              <span> CONTACT </span>
+            </nav>
+            <button className="mobile-button" onClick={this.toggleMobileMenu}> MENU ☰ </button>
+            <nav className={`mobile-nav-list ${this.state.MobileMenuClicked ?
+              "mobile-nav-list-open" : null}`}>
+              <span className="mobile-nav-list-item"> PORTFOLIO </span>
+              <span className="mobile-nav-list-item"> SERVICES </span>
+              <span className="mobile-nav-list-item"> ABOUT </span>
+              <span className="mobile-nav-list-item"> TEAM </span>
+              <span className="mobile-nav-list-item"> CONTACT </span>
+            </nav>
+          </header>
+          <div className="main-section">
+            <section className="middle-section">
+              <p className="greeting"> Welcome To Our Studio! </p>
+              <h1 className="main-greeting"> IT'S NICE TO MEET YOU </h1>
+              <button className="main-button"> TELL ME MORE </button>
+            </section>
+          </div>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
